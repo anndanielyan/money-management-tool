a18d938a26a14ca3e3f469d59afea87af61a66d3
Anna Danielyan <anna96danielyan@gmail.com>
2022-12-03 12:37:36 +0400

    added sign-in modal window

---
+++ script/modal.js
@@ -0,0 +1,25 @@
+const signIn = `
+        <h2 class="modal-headline">Sign In</h2>
+        <form action="#">
+          <label class="label-text" for="uname">Username</label>
+          <input
+            class="input-field"
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+          />
+
+          <label class="label-text" for="psw">Password</label>
+          <input
+            class="input-field"
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+          />
+
+          <button class="submit-btn" type="submit">Sign In</button>
+        </form>
+        <button class="close-modal">&times;</button>
+`;
+
+export { signIn };
---
+++ script/script.js
@@ -0,0 +1,31 @@
+import { signIn } from "./modal.js";
+
+const btnContainer = document.querySelector(".btn-container");
+const modalbox = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modalbox.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modalbox.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("sign-in")) {
+    openModal();
+    modalbox.innerHTML = signIn;
+  }
+});
+
+modalbox.addEventListener("click", function (e) {
+  if (e.target.classList.contains("close-modal")) closeModal();
+});
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modalbox.classList.contains("hidden"))
+    closeModal();
+});
