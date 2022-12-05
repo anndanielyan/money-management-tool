f525f6153f709cec10b24f3caca93a37bb24c1a2
Anna Danielyan <anna96danielyan@gmail.com>
2022-12-05 13:12:10 +0400

    eslint and prettier setup

--- script/modal.js
+++ script/modal.js
@@ -1,25 +1,25 @@
 const signIn = `
-        <h2 class="modal-headline">Sign In</h2>
-        <form action="#">
-          <label class="label-text" for="uname">Username</label>
+        <h2 class='modal-headline'>Sign In</h2>
+        <form action='#'>
+          <label class='label-text' for='uname'>Username</label>
           <input
-            class="input-field"
-            type="text"
-            placeholder="Enter Username"
-            name="uname"
+            class='input-field'
+            type='text'
+            placeholder='Enter Username'
+            name='uname'
           />
 
-          <label class="label-text" for="psw">Password</label>
+          <label class='label-text' for='psw'>Password</label>
           <input
-            class="input-field"
-            type="password"
-            placeholder="Enter Password"
-            name="psw"
+            class='input-field'
+            type='password'
+            placeholder='Enter Password'
+            name='psw'
           />
 
-          <button class="submit-btn" type="submit">Sign In</button>
+          <button class='submit-btn' type='submit'>Sign In</button>
         </form>
-        <button class="close-modal">&times;</button>
+        <button class='close-modal'>&times;</button>
 `;
 
 export { signIn };
